b2ab66f071bf5df12bb150bcbfa31ba73cfba13b
DaPipex <felipgg@gmail.com>
2015-07-18 22:09:29 -0300

    A new Start
    Mostly Taric

---
+++ PippyTaric/Program.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Color = System.Drawing.Color;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+using LConsole = LeagueSharp.Console.Console;
+using SharpDX;
+
+namespace PippyTaric
+{
+    class Program
+    {
+
+        public static bool DebugMode = false;
+
+        private const string champName = "Taric";
+        private readonly static Color pippyTaricColor = Color.FromArgb(60, 222, 203);
+        private readonly static string[] qStringList = { "Don't use", "Only on me", "Only on ally" };
+        private readonly static string buffName = "taricgemcraftbuff";
+
+        private static Spell theQ, theW, theE, theR;
+        private static Items.Item tiamat_item, hydra_item, youmuu_item, bc_item, ruined_item;
+        private static SpellSlot ignite;
+        private static Menu TaricMenu;
+        private static Orbwalking.Orbwalker penisOrb;
+        private static Obj_AI_Hero target;
+        private static Dictionary<string, float> spellInfo;
+
+        private static float animationTime;
+        private static bool TryDebug;
+        private static bool SWcombo;
+        private static bool hasPassive = false;
+
+
+
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += LoadStuff;
+        }
+
+        /*static SpellData SpellInfo(string name)
+        {
+        return SpellData.GetSpellData(name);
+        }
+        */
+
+        static void LoadStuff(EventArgs args)
+        {
+
+            if (ObjectManager.Player.ChampionName != champName)
+            {
+                return;
+            }
+
+            MySpellInfo.Initialize();
+            spellInfo = MySpellInfo.SpellTable;
+            LConsole.Show();
+
+            //#NeverForgetPrintchat
+            Notifications.AddNotification("Pippy Taric Loaded!", 10).SetTextColor(pippyTaricColor);
+
+            //Spells
+            theQ = new Spell(SpellSlot.Q, spellInfo["qRange"]);
+            theW = new Spell(SpellSlot.W, spellInfo["wRange"], TargetSelector.DamageType.Magical);
+            theE = new Spell(SpellSlot.E, spellInfo["eRange"], TargetSelector.DamageType.Magical);
+            theR = new Spell(SpellSlot.R, spellInfo["rRange"], TargetSelector.DamageType.Magical);
+
+            //Spell type
+            //theW.SetSkillshot(spellInfo["wDelay"], spellInfo["wRange"], float.PositiveInfinity, false, SkillshotType.SkillshotCircle);
+            theE.SetTargetted(spellInfo["eDelay"], spellInfo["eSpeed"]);
+            //theR.SetSkillshot(spellInfo["rDelay"], spellInfo["rRange"], float.PositiveInfinity, false, SkillshotType.SkillshotCircle);
+
+            //Do we have ignite? I mean, you, not "we" ayy lmao
+            ignite = ObjectManager.Player.GetSpellSlot("summonerdot");
+
+            //Items
+            tiamat_item = new Items.Item(3077, 400f);
+            hydra_item = new Items.Item(3074, 400f);
+            youmuu_item = new Items.Item(3142);
+            bc_item = new Items.Item(3144, 550f);
+            ruined_item = new Items.Item(31553, 550f);
+
+            //Teh menu
+            TaricMenuLoad();
+
+            //Events
+            Game.OnUpdate += TaricUpdate;
+            Drawing.OnDraw += TaricDraw;
+            Obj_AI_Hero.OnPlayAnimation += TaricAnimation;
+            Obj_AI_Hero.OnProcessSpellCast += TaricProcessSpell;
+            Orbwalking.AfterAttack += TaricAfterAttack;
+            Obj_AI_Hero.OnBuffAdd += TaricBuffAdd;
+        }
+
+        static void TaricMenuLoad()
+        {
+            TaricMenu = new Menu("Pippy Taric", "pippytaric", true);
+
+            //Orbwalker
+            var orbMenu = new Menu("Orbwalker Settings", "orbwalking");
+            penisOrb = new Orbwalking.Orbwalker(orbMenu);
+            TaricMenu.AddSubMenu(orbMenu);
+
+            //Target Selector
+            var tsMenu = new Menu("Target Selector", "targetselector");
+            TargetSelector.AddToMenu(tsMenu);
+            TaricMenu.AddSubMenu(tsMenu);
+
+            //Combo
+            var comboMenu = new Menu("Combo Settings", "combo");
+
+            comboMenu.AddItem(new MenuItem("info1", "Normal Combo Settings"));
+
+                var qComboMenu = new Menu("Q Settings", "qcombo");
+                qComboMenu.AddItem(new MenuItem("useQ", "Use Q Mode:")).SetValue(new StringList(qStringList, 2));
+                qComboMenu.AddItem(new MenuItem("useQslider", "Min Hp% to heal")).SetValue(new Slider(75, 0, 100));
+                comboMenu.AddSubMenu(qComboMenu);
+
+                var wComboMenu = new Menu("W Settings", "wcombo");
+                wComboMenu.AddItem(new MenuItem("useW", "Use W")).SetValue(true);
+                wComboMenu.AddItem(new MenuItem("useWenemies", "   on >= X enemies")).SetValue(new Slider(1, 1, 5));
+                comboMenu.AddSubMenu(wComboMenu);
+
+                var eComboMenu = new Menu("E Settings", "ecombo");
+                eComboMenu.AddItem(new MenuItem("useE", "Use E")).SetValue(true);
+                eComboMenu.AddItem(new MenuItem("useErange", "Max Range to use")).SetValue(new Slider((int)spellInfo["eRange"], 1, (int)spellInfo["eRange"]));
+                comboMenu.AddSubMenu(eComboMenu);
+
+                var rComboMenu = new Menu("R Settings", "rcombo");
+                rComboMenu.AddItem(new MenuItem("useR", "Use R")).SetValue(true);
+                rComboMenu.AddItem(new MenuItem("useRenemies", "   on >= X enemies")).SetValue(new Slider(2, 1, 5));
+                comboMenu.AddSubMenu(rComboMenu);
+
+                comboMenu.AddItem(new MenuItem("info2", string.Empty));
+
+                comboMenu.AddItem(new MenuItem("toggleCombo", "Toggle SpellWeaving Combo")).SetValue(
+                new KeyBind(90, KeyBindType.Toggle));
+                TaricMenu.AddSubMenu(comboMenu);
+
+            var harassMenu = new Menu("Harass Settings", "harass");
+            harassMenu.AddItem(new MenuItem("useQharass", "Use Q in harass (only me)")).SetValue(true);
+            harassMenu.AddItem(new MenuItem("useQharassSlider", "Min Hp% to heal")).SetValue(new Slider(75, 0, 100));
+            harassMenu.AddItem(new MenuItem("separator1", String.Empty));
+            harassMenu.AddItem(new MenuItem("useEharass", "Use E in harass (max range)")).SetValue(true);
+            TaricMenu.AddSubMenu(harassMenu);
+
+            //Healing
+            var healingMenu = new Menu("Healing Settings", "healing");
+            healingMenu.AddItem(new MenuItem("ahSelf", "Auto Heal Self")).SetValue(true);
+            healingMenu.AddItem(new MenuItem("ahSelfSlider", "at % HP")).SetValue(new Slider(70, 0, 100));
+            healingMenu.AddItem(new MenuItem("ahOther", "Auto Heal Other")).SetValue(false);
+            healingMenu.AddItem(new MenuItem("ahOtherSlider", "at % HP")).SetValue(new Slider(70, 0, 100));
+            TaricMenu.AddSubMenu(healingMenu);
+
+            //Drawing
+            var drawMenu = new Menu("Drawing Settings", "draw");
+            drawMenu.AddItem(new MenuItem("qRangeDraw", "Draw Q Range")).SetValue(new Circle(true, pippyTaricColor));
+            drawMenu.AddItem(new MenuItem("wRangeDraw", "Draw W Range")).SetValue(new Circle(true, pippyTaricColor));
+            drawMenu.AddItem(new MenuItem("eRangeDrawMax", "Draw E MAX Range")).SetValue(new Circle(false, Color.Red));
+            drawMenu.AddItem(new MenuItem("eRangeDraw", "Draw set E range")).SetValue(new Circle(true, pippyTaricColor));
+            drawMenu.AddItem(new MenuItem("rRangeDraw", "Draw R Range")).SetValue(new Circle(true, pippyTaricColor));
+            drawMenu.AddItem(new MenuItem("drawHide", "Hide Ranges if not available")).SetValue(false);
+            drawMenu.AddItem(new MenuItem("drawTarget", "Draw Orbwalker Target")).SetValue(true);
+            TaricMenu.AddSubMenu(drawMenu);
+
+            TaricMenu.AddItem(new MenuItem("doDebug", "Debug Stuff?")).SetValue(true);
+
+            TaricMenu.AddToMainMenu();
+        }
+
+        private static void TaricUpdate(EventArgs args)
+        {
+            target = TargetSelector.GetTarget(GetTargetRange(), TargetSelector.DamageType.Magical);
+
+            TryDebug = TaricMenu.Item("doDebug").GetValue<bool>();
+
+            SWcombo = TaricMenu.Item("toggleCombo").GetValue<KeyBind>().Active;
+
+            //animationTime = ObjectManager.Player.AttackCastDelay;
+
+            hasPassive = ObjectManager.Player.HasBuff(buffName);
+
+            switch (penisOrb.ActiveMode)
+            {
+                case Orbwalking.OrbwalkingMode.Combo:
+                    if (!SWcombo)
+                    {
+                        Combo();
+                    }
+                    else
+                    {
+                        SpellWeaving();
+                    }
+                    break;
+                case Orbwalking.OrbwalkingMode.Mixed:
+                    Harass();
+                    break;
+                case Orbwalking.OrbwalkingMode.LaneClear:
+                    //LaneClear();
+                    break;
+            }
+
+            if (TryDebug)
+            {
+                //LConsole.WriteLine(hasPassive);
+            }
+        }
+
+        private static float GetTargetRange()
+        {
+            var eValue = TaricMenu.Item("useErange").GetValue<Slider>().Value;
+            var wValue = spellInfo["wRange"];
+
+            if (eValue >= wValue)
+            {
+                return eValue;
+            }
+
+            return wValue;
+        }
+
+        private static void Combo()
+        {
+
+            var allyList = ObjectManager.Get<Obj_AI_Hero>().ToList().FindAll(ally => ally.IsAlly);
+            var enemyList = ObjectManager.Get<Obj_AI_Hero>().ToList().FindAll(enemy => enemy.IsEnemy);
+
+            if (TryDebug)
+            {
+                //LConsole.WriteLine(TaricMenu.Item("useQ").GetValue<StringList>().SelectedIndex);
+                //LConsole.WriteLine("My health percentage : " + ObjectManager.Player.HealthPercent);
+            }
+
+            if (target == null)
+            {
+                return;
+            }
+
+            if (theQ.IsReady())
+            {
+                switch (TaricMenu.Item("useQ").GetValue<StringList>().SelectedIndex)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        if (ObjectManager.Player.HealthPercent <= TaricMenu.Item("useQslider").GetValue<Slider>().Value)
+                        {
+                            theQ.CastOnUnit(ObjectManager.Player);
+                        }
+                        break;
+                    case 2:
+                        foreach (Obj_AI_Hero ally in allyList)
+                        {
+                            if (!ally.IsDead)
+                            {
+                                if (ally.HealthPercent <= TaricMenu.Item("useQslider").GetValue<Slider>().Value)
+                                {
+                                    theQ.CastOnUnit(ally);
+                                }
+                            }
+                        }
+                        break;
+                }
+            }
+
+            if (theW.IsReady())
+            {
+                if (TaricMenu.Item("useW").GetValue<bool>())
+                {
+                    if (ObjectManager.Player.CountEnemiesInRange(spellInfo["wRange"]) >= TaricMenu.Item("useWenemies")//fag
+                        .GetValue<Slider>().Value)
+                    {
+                        theW.Cast();
+                    }
+                }
+            }
+
+            if (theE.IsReady())
+            {
+                if (TaricMenu.Item("useE").GetValue<bool>())
+                {
+                    if (ObjectManager.Player.ServerPosition.Distance(target.ServerPosition) <= TaricMenu.Item("useErange")//fag
+                        .GetValue<Slider>().Value)
+                    {
+                        theE.Cast(target);
+                    }
+                }
+            }
+
+            if (theR.IsReady())
+            {
+                if (TaricMenu.Item("useR").GetValue<bool>())
+                {
+                    if (ObjectManager.Player.CountEnemiesInRange(spellInfo["rRange"]) >= TaricMenu.Item("useRenemies")//fag
+                        .GetValue<Slider>().Value)
+                    {
+                        theR.Cast();
+                    }
+                }
+            }
+        }
+
+        private static void Harass()
+        {
+            if (target == null)
+            {
+                return;
+            }
+
+            if (theQ.IsReady())
+            {
+                if (TaricMenu.Item("useQharass").GetValue<bool>())
+                {
+                    if (ObjectManager.Player.HealthPercent <= TaricMenu.Item("useQharassSlider").GetValue<Slider>().Value)
+                    {
+                        theQ.Cast(ObjectManager.Player);
+                    }
+                }
+            }
+
+            if (theE.IsReady())
+            {
+                if (TaricMenu.Item("useEharass").GetValue<bool>())
+                {
+                    if (ObjectManager.Player.ServerPosition.Distance(target.ServerPosition) <= spellInfo["eRange"])
+                    {
+                        theE.Cast(target);
+                    }
+                }
+            }
+        }
+
+        private static void SpellWeaving()
+        {
+            if (SWcombo && penisOrb.ActiveMode == Orbwalking.OrbwalkingMode.Combo && target != null)
+            {
+                if (theE.IsReady()
+                && target.IsValidTarget(spellInfo["eRange"]) && !GetPassiveState())
+                {
+                    theE.CastOnUnit(target);
+                }
+                else if (theR.IsReady() && !theE.IsReady()
+                && target.IsValidTarget(spellInfo["rRange"]) && !GetPassiveState())
+                {
+                    theR.Cast();
+                }
+                else if (theW.IsReady() && !theR.IsReady() && !theE.IsReady()
+                && target.IsValidTarget(spellInfo["wRange"]) && !GetPassiveState())
+                {
+                    theW.Cast();
+                }
+                else if (theQ.IsReady() && !theW.IsReady() && !theR.IsReady() && !theE.IsReady()
+                && target.IsValidTarget(spellInfo["qRange"]) && !GetPassiveState())
+                {
+                    theQ.CastOnUnit(ObjectManager.Player);
+                }
+            }
+        }
+
+        private static void TaricDraw(EventArgs args)
+        {
+
+        }
+
+        private static void TaricAnimation(GameObject sender, GameObjectPlayAnimationEventArgs args)
+        {
+
+        }
+
+        private static void TaricProcessSpell(GameObject sender, GameObjectProcessSpellCastEventArgs args)
+        {
+
+        }
+
+        private static void TaricAfterAttack(AttackableUnit unit, AttackableUnit target)
+        {
+
+        }
+
+        private static void TaricBuffAdd(Obj_AI_Base sender, Obj_AI_BaseBuffAddEventArgs args)
+        {
+            if (TryDebug)
+            {
+                if (sender.IsMe && args.Buff.Name != "shatterauraself")
+                {
+                    LConsole.WriteLine("Buff acquired: " + args.Buff.Name.ToString());
+                }
+            }
+
+            if (sender.IsMe && args.Buff.Name == buffName)
+            {
+                hasPassive = true;
+            }
+        }
+
+        private static void TaricBuffRemove(Obj_AI_Base sender, Obj_AI_BaseBuffRemoveEventArgs args)
+        {
+            if (sender.IsMe && args.Buff.Name == buffName)
+            {
+                hasPassive = false;
+            }
+        }
+
+        private static bool GetPassiveState()
+        {
+            return hasPassive;
+        }
+    }
+}
